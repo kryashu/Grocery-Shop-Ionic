d163d4f3d5c8857cfbc90683082074804a1160be
Aarm <avinashmaurya84@gmail.com>
2020-08-04 21:44:06 +0530

    make home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,15 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import {SigninPageModule} from "./signin/signin.module";
+import {TabsPageModule} from "./tabs/tabs.module";
 
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
-  SigninPageModule],
+  SigninPageModule,
+  TabsPageModule],
   providers: [
     StatusBar,
     SplashScreen,
---
+++ src/app/tabs/tabs-routing.module.ts
@@ -0,0 +1,69 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { TabsPage } from './tabs.page';
+
+const routes: Routes = [
+  {
+    path: 'tabs',
+    component: TabsPage,
+    children: [
+      {
+        path: 'homepage',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('../homepage/homepage.module').then( m => m.HomepagePageModule)
+          }
+        ]
+      },
+      {
+        path: 'wishlist-tab',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('../wishlist-tab/wishlist-tab.module').then( m => m.WishlistTabPageModule)
+          }
+        ]
+      },
+      {
+        path: 'search-tab',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('../search-tab/search-tab.module').then( m => m.SearchTabPageModule)
+          }
+        ]
+      },
+      {
+        path: 'cart-tab',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('../cart-tab/cart-tab.module').then( m => m.CartTabPageModule)
+          }
+        ]
+      },
+      {
+        path: 'profile-tab',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('../profile-tab/profile-tab.module').then( m => m.ProfileTabPageModule)
+          }
+        ]
+      },
+      {
+        path: '',
+        redirectTo: '/tabs/homepage',
+        pathMatch: 'full'
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class TabsPageRoutingModule {}
