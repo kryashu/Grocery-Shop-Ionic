57b11360bc8b1144549515cacb50531bbd10817d
Aarm <avinashmaurya84@gmail.com>
2020-08-04 13:46:47 +0530

    make signin page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-import {SignupSigninComponent} from "./signup-signin/signup-signin.component";
 
 const routes: Routes = [
   {
@@ -15,7 +14,27 @@ const routes: Routes = [
   {
     path: 'sign',
     loadChildren: () => import('./sign/sign.module').then( m => m.SignPageModule)
-  }
+  },
+  {
+    path: 'sign-with-mobile',
+    loadChildren: () => import('./sign-with-mobile/sign-with-mobile.module').then( m => m.SignWithMobilePageModule)
+  },
+  {
+    path: 'verify-otp',
+    loadChildren: () => import('./verify-otp/verify-otp.module').then( m => m.VerifyOtpPageModule)
+  },
+  {
+    path: 'homepage',
+    loadChildren: () => import('./homepage/homepage.module').then( m => m.HomepagePageModule)
+  },
+  // {
+  //   path: 'signup',
+  //   loadChildren: () => import('./signup/signup.module').then( m => m.SignupPageModule)
+  // },
+  // {
+  //   path: 'signin',
+  //   loadChildren: () => import('./signin/signin.module').then( m => m.SigninPageModule)
+  // }
 ];
 
 @NgModule({
---
+++ src/app/sign-with-mobile/sign-with-mobile.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-with-mobile',
+  templateUrl: './sign-with-mobile.page.html',
+  styleUrls: ['./sign-with-mobile.page.scss'],
+})
+export class SignWithMobilePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
