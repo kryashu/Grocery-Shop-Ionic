04f035df4b7b8f584db9ab5fe731195fdba6bf27
krarchi <kumar.archi.anand@symbtechnologies.com>
2020-08-06 11:03:35 +0530

    Backbutton exit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
-import {Router} from "@angular/router";
+import {Router} from '@angular/router';
+import {Platform} from 'ionic-angular/umd';
 
 @Component({
   selector: 'app-home',
@@ -8,23 +9,26 @@ import {Router} from "@angular/router";
 })
 export class HomePage implements OnInit {
   loaderValue;
-  constructor(  private router: Router,) {
-
+  constructor(  private router: Router,
+                private platform: Platform) {
+    this.exit();
   }
 
 
   ngOnInit(): void {
     this.startTimer();
   }
+
+  exit(){
+    this.platform.backButton.subscribe(async () => {
+    this.platform.exitApp(); });
+  }
+
   startTimer() {
-    console.log('start')
+    console.log('start');
     this.loaderValue = setTimeout(() => {
       // my to do
       this.router.navigate(['sign']);
     }, 3000);
-    //
-    // setTimeout(() => {
-    //   this.router.navigate(['tabs', 'homepage']);
-    // }, 30000);
   }
 }
