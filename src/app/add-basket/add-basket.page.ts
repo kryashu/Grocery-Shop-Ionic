4f2fec1d0ed7ca902d37c8c981d3011269c17309
krarchi <kumar.archi.anand@symbtechnologies.com>
2020-08-11 17:00:14 +0530

    Changes in UI

--- src/app/add-basket/add-basket.page.ts
+++ src/app/add-basket/add-basket.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
 import {ModalController} from '@ionic/angular';
-
+import { ToastController } from '@ionic/angular';
 @Component({
   selector: 'app-add-basket',
   templateUrl: './add-basket.page.html',
@@ -13,20 +13,34 @@ export class AddBasketPage implements OnInit {
   totalPrice = 0;
   itemList = [{name: '24cm(1 person)', count: 0, price: 4.50}, {name: '32cm(2-3 persons)', count: 0, price: 6.50}, {name: '40cm(3-4 persons)', count: 0, price: 6.50}];
   constructor(private router: Router,
-              public modalController: ModalController) { }
+              public modalController: ModalController,
+              private toaster: ToastController) { }
 
   ngOnInit() {
   }
+  async presentToast() {
+    const toast = await this.toaster.create({
+      message: 'Please select an item.',
+      color: 'danger',
+      cssClass: 'toaster',
+      duration: 2000
+    });
+    toast.present();
+  }
   add(){
+    if (this.selectedItem){
     for (const item of this.itemList){
       if (item.name === this.selectedItem){
         item.count += 1;
         this.totalPrice += item.price;
         this.itemCount += 1;
       }
+    }}else{
+        this.presentToast();
     }
   }
   remove(){
+    if (this.selectedItem){
     for (const item of this.itemList){
       if (item.name === this.selectedItem){
         if (item.count > 0) {
@@ -39,6 +53,8 @@ export class AddBasketPage implements OnInit {
           }
         }
       }
+    }}else{
+      this.presentToast();
     }
   }
   submit(){
