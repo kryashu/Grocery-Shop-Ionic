94209c5f67a50ed5d1f4f0002a630e3fa2d4a6af
krarchi <kumar.archi.anand@symbtechnologies.com>
2020-08-12 15:40:06 +0530

    Bug fixes

--- src/app/homepage/homepage.page.ts
+++ src/app/homepage/homepage.page.ts
@@ -1,10 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
 import { ToastController } from '@ionic/angular';
-import { Plugins } from '@capacitor/core';
-import { BackButtonEvent } from '@ionic/core';
 import { Platform } from '@ionic/angular';
-const { App } = Plugins;
+
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.page.html',
@@ -15,13 +13,14 @@ export class HomepagePage implements OnInit {
   constructor(private router: Router,
               private toastController: ToastController,
               private platform: Platform) {
-    this.subscribe = this.platform.backButton.subscribeWithPriority(666666, () =>{
-      if (this.constructor.name === 'HomepagePage'){
-        if (window.confirm('Are you sure you want to exit?')){
-          // tslint:disable-next-line
-          navigator["app"].exitApp();
+    this.router.events.subscribe((e) => {
+        if (this.router.url === '/tabs/homepage') {
+          this.subscribe = this.platform.backButton.subscribeWithPriority(666666, () =>{
+            if (this.router.url === '/tabs/homepage'){
+              this.presentToastWithOptions();
+            }
+          });
         }
-      }
     });
   }
 
@@ -30,26 +29,27 @@ export class HomepagePage implements OnInit {
   openViewAll(name){
     this.router.navigate(['/view-all', {value: name}]);
   }
-  // async presentToastWithOptions() {
-  //   const toast = await this.toastController.create({
-  //     header: 'Untouched App',
-  //     message: 'Are you sure u want to exit?',
-  //     buttons: [
-  //       {
-  //         text: 'Cancle',
-  //         role: 'cancle',
-  //         handler: () => {
-  //           toast.dismiss();
-  //         }
-  //       }, {
-  //         text: 'Ok',
-  //         handler: () => {
-  //           window.close();
-  //         }
-  //       }
-  //     ]
-  //   });
-  //   toast.present();
-  // }
+  async presentToastWithOptions() {
+    const toast = await this.toastController.create({
+      header: 'Untouched App',
+      message: 'Are you sure u want to exit?',
+      buttons: [
+        {
+          text: 'Cancle',
+          role: 'cancle',
+          handler: () => {
+            toast.dismiss();
+          }
+        }, {
+          text: 'Ok',
+          handler: () => {
+            // tslint:disable-next-line
+            navigator["app"].exitApp();
+          }
+        }
+      ]
+    });
+    toast.present();
+  }
 
 }
