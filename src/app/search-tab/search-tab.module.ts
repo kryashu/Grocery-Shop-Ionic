9a1c50dde827be7f8c5fa38f302c5b959aeabb20
krarchi <kumar.archi.anand@symbtechnologies.com>
2020-08-12 20:04:20 +0530

    Slide 49 added

--- src/app/search-tab/search-tab.module.ts
+++ src/app/search-tab/search-tab.module.ts
@@ -7,13 +7,18 @@ import { IonicModule } from '@ionic/angular';
 import { SearchTabPageRoutingModule } from './search-tab-routing.module';
 
 import { SearchTabPage } from './search-tab.page';
+import { AgmCoreModule } from '@agm/core';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    SearchTabPageRoutingModule
+    SearchTabPageRoutingModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyAClE7_4DBppqt1JDCG_21LQ2qWMXHFNaw',
+      libraries: ['places']
+    })
   ],
   declarations: [SearchTabPage]
 })
--- src/app/search-tab/search-tab.page.ts
+++ src/app/search-tab/search-tab.page.ts
@@ -1,15 +1,86 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild, ElementRef, NgZone } from '@angular/core';
+import { MapsAPILoader, AgmMap } from '@agm/core';
 @Component({
   selector: 'app-search-tab',
   templateUrl: './search-tab.page.html',
   styleUrls: ['./search-tab.page.scss'],
 })
 export class SearchTabPage implements OnInit {
-
-  constructor() { }
+  latitude: number;
+  longitude: number;
+  zoom: number;
+  address: string;
+  private geoCoder;
+  // tslint:disable-next-line:max-line-length
+  storeList = [{name: 'More Market', tag: 'Medical, Confectionary, European, International…', star: '4.8(120+)', time: '60', cost: 'Free'},{name: 'More Market', tag: 'Medical, Confectionary, European, International…', star: '4.8(120+)', time: '60', cost: 'Free'}];
+  caardList = [{name: '$1000 Monthly Gift Card', tag: 'Lorem Ipsum', star: '4.8(120+)', time: '60', cost: 'Free'},{name: '$1000 Monthly Gift Card', tag: 'Lorem Ipsum', star: '4.8(120+)', time: '60', cost: 'Free'}];
+  @ViewChild('search',{static:false})
+  public searchElementRef: ElementRef;
+  constructor( private mapsAPILoader: MapsAPILoader,
+               private ngZone: NgZone) { }
 
   ngOnInit() {
+    // //load Places Autocomplete
+    // this.mapsAPILoader.load().then(() => {
+    //   this.setCurrentLocation();
+    //   this.geoCoder = new google.maps.Geocoder;
+    //
+    //   let autocomplete = new google.maps.places.Autocomplete(this.searchElementRef.nativeElement, {
+    //     types: ["address"]
+    //   });
+    //   autocomplete.addListener("place_changed", () => {
+    //     this.ngZone.run(() => {
+    //       //get the place result
+    //       let place: google.maps.places.PlaceResult = autocomplete.getPlace();
+    //
+    //       //verify result
+    //       if (place.geometry === undefined || place.geometry === null) {
+    //         return;
+    //       }
+    //
+    //       //set latitude, longitude and zoom
+    //       this.latitude = place.geometry.location.lat();
+    //       this.longitude = place.geometry.location.lng();
+    //       this.zoom = 12;
+    //     });
+    //   });
+    // });
+  }
+  // Get Current Location Coordinates
+  private setCurrentLocation() {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.latitude = position.coords.latitude;
+        this.longitude = position.coords.longitude;
+        this.zoom = 8;
+        this.getAddress(this.latitude, this.longitude);
+      });
+    }
   }
 
+
+  markerDragEnd($event: any) {
+    console.log($event);
+    this.latitude = $event.coords.lat;
+    this.longitude = $event.coords.lng;
+    this.getAddress(this.latitude, this.longitude);
+  }
+
+  getAddress(latitude, longitude) {
+    this.geoCoder.geocode({ 'location': { lat: latitude, lng: longitude } }, (results, status) => {
+      console.log(results);
+      console.log(status);
+      if (status === 'OK') {
+        if (results[0]) {
+          this.zoom = 12;
+          this.address = results[0].formatted_address;
+        } else {
+          window.alert('No results found');
+        }
+      } else {
+        window.alert('Geocoder failed due to: ' + status);
+      }
+
+    });
+  }
 }
