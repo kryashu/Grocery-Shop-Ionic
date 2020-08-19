5504149c417182cccf067d5517e46cd02ad3c0ad
Aarm <avinashmaurya84@gmail.com>
2020-08-19 14:30:03 +0530

    address ui issue

--- src/app/new-address/new-address.page.ts
+++ src/app/new-address/new-address.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
 import {Location} from '@angular/common';
+import {ToastController} from "@ionic/angular";
 
 @Component({
   selector: 'app-new-address',
@@ -9,9 +10,31 @@ import {Location} from '@angular/common';
 })
 export class NewAddressPage implements OnInit {
   input1 = 'Bulevardul Dimitrie Cantemir 49B';
+  input2;
+  input3;
   constructor(private router: Router,
-              private location: Location) { }
+              private location: Location,
+              public toastController: ToastController) { }
 
   ngOnInit() {
   }
+
+  saveAddress() {
+    if (this.input1 === '' || this.input2 === '' || this.input3 === '' || this.input3 === undefined || this.input2 === undefined || this.input1 === undefined  ) {
+    this.change();
+    return false
+    } else  {
+      this.router.navigate(['tabs','profile-tab'])
+      return true
+    }
+  }
+  async change(){
+    const toast = await this.toastController.create({
+      message: 'Plese fill full address',
+      duration: 2000,
+      cssClass: 'Toast'
+    });
+    await toast.present();
+  }
+
 }
