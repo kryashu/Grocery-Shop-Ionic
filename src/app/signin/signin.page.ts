0917c768d6b2056a0239c7df5df8d5578d8dd21b
krarchi <kumar.archi.anand@symbtechnologies.com>
2020-08-19 16:18:31 +0530

    fixes in google login

--- src/app/signin/signin.page.ts
+++ src/app/signin/signin.page.ts
@@ -17,7 +17,10 @@ export class SigninPage implements OnInit {
   ngOnInit() {
   }
 signInWithGoogle(){
-  this.googlePlus.login({})
+  this.googlePlus.login({	scopes: '', // optional, space-separated list of scopes, If not included or empty, defaults to `profile` and `email`.
+      // tslint:disable-next-line:max-line-length
+      webClientId: '264852184912-dq35e4hns7hga54nvmd8osnf0oa9ujq2.apps.googleusercontent.com', // optional clientId of your Web application from Credentials settings of your project - On Android, this MUST be included to get an idToken. On iOS, it is not required.
+      offline: true})
       .then(res => {
         console.log(res);
         alert(res);
