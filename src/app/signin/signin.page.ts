7272ea4b4c073481b39b9cd297bd3ee156427f5d
krarchi <kumar.archi.anand@symbtechnologies.com>
2020-08-21 18:57:42 +0530

    fixes for social login

--- src/app/signin/signin.page.ts
+++ src/app/signin/signin.page.ts
@@ -1,38 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
-import { GooglePlus } from '@ionic-native/google-plus/ngx';
-import { Facebook, FacebookLoginResponse } from '@ionic-native/facebook/ngx';
+import {GooglePlus} from '@ionic-native/google-plus/ngx';
+import {Facebook, FacebookLoginResponse} from '@ionic-native/facebook/ngx';
 
 @Component({
-  selector: 'app-signin',
-  templateUrl: './signin.page.html',
-  styleUrls: ['./signin.page.scss'],
+    selector: 'app-signin',
+    templateUrl: './signin.page.html',
+    styleUrls: ['./signin.page.scss'],
 })
 export class SigninPage implements OnInit {
 
-  constructor(public router: Router,
-              private googlePlus: GooglePlus,
-              private fb: Facebook) { }
+    constructor(public router: Router,
+                private googlePlus: GooglePlus,
+                private fb: Facebook) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
-signInWithGoogle(){
-    // tslint:disable-next-line:max-line-length
-  this.googlePlus.login({})
-      .then(res => {
-        console.log(res);
-        this.router.navigate(['tabs', 'homepage']);
-      })
-      .catch(err => alert(err));
-}
+    signInWithGoogle() {
+        // tslint:disable-next-line:max-line-length
+        this.googlePlus.login({})
+            .then(res => {
+                console.log(res);
+                alert(JSON.stringify(res));
+                this.router.navigate(['tabs', 'homepage']);
+            })
+            .catch(err => alert(err));
+    }
 
-signInWithFB(){
-  this.fb.login(['public_profile', 'user_friends', 'email'])
-      .then((res: FacebookLoginResponse) => {
-        console.log('Logged into Facebook!', res);
-        this.router.navigate(['tabs', 'homepage']);
-      } )
-      .catch(e => console.log('Error logging into Facebook', e));
-  }
+    signInWithFB() {
+        this.fb.login(['public_profile', 'user_friends', 'email'])
+            .then((res: FacebookLoginResponse) => {
+                console.log('Logged into Facebook!', res);
+                alert(JSON.stringify(res));
+                this.router.navigate(['tabs', 'homepage']);
+            })
+            .catch(e => console.log('Error logging into Facebook', e));
+    }
 }
