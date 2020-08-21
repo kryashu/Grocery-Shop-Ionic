83e9b3bc490c5cdb27ab87c74f119ee91e089590
krarchi <kumar.archi.anand@symbtechnologies.com>
2020-08-21 13:04:05 +0530

    added checkout page

--- src/app/checkout/checkout.page.ts
+++ src/app/checkout/checkout.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'app-checkout',
@@ -7,8 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CheckoutPage implements OnInit {
 
-  constructor() { }
-
+  constructor(private router: Router,
+              private location: Location) { }
+  itemList = [{
+    name: 'MDH Masala',
+    quantity: '200 gms',
+    originalPrice: `19.50`,
+    discount: 2.50,
+    discountedPrice: 17.00,
+    count: 1
+  }, {name: 'Chunky Chat Masala', quantity: '500 gms', originalPrice: '19.50', discount: 3.50, discountedPrice: 16.00, count: 1}];
   ngOnInit() {
   }
 
